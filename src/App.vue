2fac58c38b75cafacf770ab44e3005972b713257
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-10 14:00:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1 class="text-center">Prova!</h1>
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss' as *;
+@use './assets/scss/vars.scss' as *;
+
+h1 {
+  color: $secondary-color;
+}
+</style>
