66caf6be62147033f6e7bdb5d8e9fbbdc23e485a
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-13 10:18:48 +0200

    AppTable

--- src/components/AppFooter_components/AppForm.vue
+++ src/components/AppFooter_components/AppForm.vue
@@ -7,14 +7,23 @@ export default {
 <template>
   <section id="form">
     <div>
-      Placeholder
+      <h2>Subscribe <span>&</span> Receive A Free eBook</h2>
+      <p class="text-center">Aliquam erat volutpat. Ut quis ligula a magna blandit finibus. Suspendisse maximus lacus non
+        nunc lacinia
+        lobortis.</p>
+      <div class="d-flex align-items-center">
+        <div class="pe-5">
+          <input type="email" class="form-control" placeholder="insert your email...*">
+        </div>
+        <button type="submit" class="btn">SUBSCRIBE</button>
+      </div>
     </div>
   </section>
   <section class="d-flex">
-    <figure>Placeholder</figure>
-    <figure>Placeholder</figure>
-    <figure>Placeholder</figure>
-    <figure>Placeholder</figure>
+    <figure><img src="../../assets/img/Yogurt-Nan-400x263.jpg" alt="Yogurt Nan"></figure>
+    <figure><img src="../../assets/img/Mixed-fruits-400x263.jpg" alt="Mixed Fruits"></figure>
+    <figure><img src="../../assets/img/r-rachel-park-366508-unsplash-min-400x263.jpg" alt="Rachel Park"></figure>
+    <figure><img src="../../assets/img/r-michelle-tsang-500721-unsplash-min-400x263.jpg" alt="Michelle Tsang"></figure>
   </section>
 </template>
 
@@ -26,16 +35,18 @@ export default {
   height: 500px;
   margin-top: 80px;
   @include flex_center;
-
-  // temporanei
-  background-image: url(../../assets/img/3a74ce3d0532b7773b174c45ca3bd05a_bigger.png);
-  border: $debug;
+  background-image: url(../../assets/img/subscribe-sec-bg.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: $primary-color;
 
   >div {
     height: 300px;
     width: 800px;
     background-color: $primary-color;
     @include flex_center;
+    flex-direction: column;
   }
 }
 
@@ -44,5 +55,38 @@ figure {
   height: 220px;
   border: $debug;
   margin: 0;
+
+  img {
+    object-fit: cover;
+    height: 100%;
+  }
+}
+
+h2 {
+  font-family: Vidaloka;
+
+  span {
+    color: $secondary-color;
+  }
+}
+
+p {
+  padding: 20px 50px;
+  color: $tertiary-color;
+  font-size: 17px;
+}
+
+input {
+  width: 500px;
+}
+
+button {
+  background-color: $secondary-color;
+  color: $primary-color;
+  font-weight: 600;
+  font-size: 14px;
+  border-radius: 0;
+  padding: 8px 20px;
+  cursor: pointer;
 }
 </style>
--- src/components/AppMain_components/AppTable.vue
+++ src/components/AppMain_components/AppTable.vue
@@ -8,8 +8,14 @@ export default {
 <template>
   <section class="container">
     <div class="row">
-      <div class="col d-flex justify-content-start">Placeholder</div>
-      <div class="col d-flex justify-content-end">Placeholder</div>
+      <div class="col d-flex justify-content-start flex-column">
+        <h5>FARM TO TABLE</h5>
+        <p>Aliquam erat volutpat. Ut quis ligula a magna blandit finibus. Suspendisse maximus lacus
+          non nunc lacinia lobortis.</p>
+      </div>
+      <div class="col d-flex justify-content-end align-items-start">
+        <a href="#">READ OUR BLOG <i class="fa-solid fa-book-open-reader ps-2"></i></a>
+      </div>
     </div>
     <AppTableContents />
   </section>
@@ -17,6 +23,7 @@ export default {
 
 <style lang="scss" scoped>
 @use '../../assets/scss/mixins.scss' as *;
+@use '../../assets/scss/vars' as *;
 
 section {
   margin-top: 80px;
@@ -24,5 +31,24 @@ section {
   >div {
     height: 150px;
   }
+}
+
+.row>div {
+
+  p {
+    padding: 20px 0;
+    color: $tertiary-color;
+    font-size: 17px;
+  }
+}
+
+a {
+  background-color: $secondary-color;
+  color: $primary-color;
+  font-weight: 600;
+  font-size: 14px;
+  border-radius: 0;
+  padding: 8px 20px;
+  cursor: pointer;
 }
 </style>
